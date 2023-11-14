f0586aca9f52c3b2e7ec0f23aa587a9ea62aef3f
Gabriel BS <paulogabriel.bsm@gmail.com>
2023-11-14 16:25:23 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './assets/components/Header.jsx'
+import Main from './assets/components/Main.jsx'
+
 import "./sass/App.sass"
 
 function App() {
   return (
     <>
         <Header />
+        <Main />
     </>
   )
 }
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -6,27 +6,47 @@ import Relogio from "./img/relogio.svg"
 
 export default function Header() {
 
-
     return(
         <>
-        <header>
-            
-                <img src={Logo} alt="Logo Next Vale" />
-                <ul>
-                    <li className="header-infomation">
-                        <img src={Maps} alt="" />
-                        <p>Rua Frans de Castro Holzwarth, 57 - sala 105Jd. Pereira do Amparo - Jacareí - SP 12327-696</p>
+        <header className="header">  
+            <div className="header-superior">
+                <img className="logo" src={Logo} alt="Logo Next Vale" />
+                <ul className="header-superior-lista">
+                    <li className="header-superior-lista-itens">
+                        <a href="" className="header-superior-lista-itens-ancora">
+                            <img className="icon" src={Maps} alt="" />
+                            <p className="informacoes">Rua Frans de Castro Holzwarth, 57 - sala 105 <br />Jd. Pereira do Amparo - Jacareí - SP 12327-696</p>
+                        </a>
                     </li>
-                    <li>
-                        <img src={Whatsapp} alt="" />
-                        <img src={Telefone} alt="" />
-                        <p>(12) 98113-2013</p>
+                    <li className="header-superior-lista-itens">
+                        <a href="" className="header-superior-lista-itens-ancora">
+                            <img className="icon" src={Whatsapp} alt="" />
+                            <p className="informacoes">(12) 98113-2013</p>
+                        </a>
                     </li>
-                    <li>
-                        <img src={Relogio} alt="" />
-                        <p></p>
+                    <li className="header-superior-lista-itens">
+                        <a href="" className="header-superior-lista-itens-ancora">
+                            <img className="icon" src={Telefone} alt="" />
+                            <p className="informacoes">(12) 98113-2013</p>
+                        </a>
+                    </li>
+                    <li className="header-superior-lista-itens">
+                        <a href="" className="header-superior-lista-itens-ancora">
+                            <img className="icon" src={Relogio} alt="" />
+                            <p className="informacoes">Seg - Sex: 08:00 - 17:00</p>
+                        </a>
                     </li>
                 </ul>
+            </div>
+            <nav  className="header-inferior">
+                <ul className="header-inferior-list">
+                    <li className="header-inferior-list-itens"><a className="header-inferior-list-itens-ancora" href="">Inicio</a></li>
+                    <li className="header-inferior-list-itens"><a className="header-inferior-list-itens-ancora" href="">Sobre</a></li>
+                    <li className="header-inferior-list-itens"><a className="header-inferior-list-itens-ancora" href="">Cabeamento Metálico e Fibra Óptica</a></li>
+                    <li className="header-inferior-list-itens"><a className="header-inferior-list-itens-ancora" href="">Consultoria e Projetos</a></li>
+                    <li className="header-inferior-list-itens"><a className="header-inferior-list-itens-ancora" href="">Contato</a></li>
+                </ul>
+            </nav>
         </header>
         </>
     )
