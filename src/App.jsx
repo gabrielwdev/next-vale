bf8272876477618dd6680cb190ba51c3f0949838
gabriel <unicodigo.br@gmail.com>
2023-12-06 16:44:21 -0300

    stn solucoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './assets/components/Header.jsx'
 import Main from './assets/components/Main.jsx'
 import Solucoes from './assets/components/Stn_solucoes.jsx'
+import Quem_somos from './assets/components/Stn_quem_somos.jsx'
 
 
 import "./sass/App.sass"
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Main />
         <Solucoes />
+        <Quem_somos />
     </>
   )
 }
--- src/assets/components/Stn_solucoes.jsx
+++ src/assets/components/Stn_solucoes.jsx
@@ -1,17 +1,61 @@
+import img from "./img/datacenter.jpg";
+
 export default function Stn_solucoes() {
-    return(
-        <>
-        <section>
-            <h2></h2>
-            <ul className="list-cards">
-                <li className="list-cards-card">
-                    <img src="" alt="" className="img" />
-                    <h3  className="titulo"></h3>
-                    <p className="descricao"></p>
-                </li>
-            </ul>
-            <input type="button" value="Entre em Contato Conosco" />
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section className="stn-solucoes">
+        <h2 className="stn-solucoes-title">SOLUÇÕES OFERECIDAS:</h2>
+        <ul className="list-cards">
+          <li className="list-cards-card">
+            <img src={img} alt="" className="img" />
+            <h3 className="titulo">Datacenter</h3>
+            <p className="descricao">
+              Montagem de infra-estrutura <br /> de alta complexidade.
+            </p>
+          </li>
+        
+        
+          <li className="list-cards-card">
+            <img src={img} alt="" className="img" />
+            <h3 className="titulo">Datacenter</h3>
+            <p className="descricao">
+              Montagem de infra-estrutura <br /> de alta complexidade.
+            </p>
+          </li>
+        
+        
+          <li className="list-cards-card">
+            <img src={img} alt="" className="img" />
+            <h3 className="titulo">Datacenter</h3>
+            <p className="descricao">
+              Montagem de infra-estrutura <br /> de alta complexidade.
+            </p>
+          </li>
+        
+        
+          <li className="list-cards-card">
+            <img src={img} alt="" className="img" />
+            <h3 className="titulo">Datacenter</h3>
+            <p className="descricao">
+              Montagem de infra-estrutura <br /> de alta complexidade.
+            </p>
+          </li>
+        
+        
+          <li className="list-cards-card par">
+            <img src={img} alt="" className="img" />
+            <h3 className="titulo">Datacenter</h3>
+            <p className="descricao">
+              Montagem de infra-estrutura <br /> de alta complexidade.
+            </p>
+          </li>
+        
+        
+
+        </ul>
+        
+        <input type="button" className="btn" value="Entre em Contato Conosco" />
+      </section>
+    </>
+  );
 }
